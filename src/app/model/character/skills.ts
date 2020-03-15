ffe4c816f871442b7a31fe4e11c94502d9b27e66
Thomas Henage <thenage@gmail.com>
2020-03-15 16:45:50 -0600

    Added Skill Names

--- src/app/model/character/skills.ts
+++ src/app/model/character/skills.ts
@@ -1,5 +1,22 @@
 interface SkillsData {
-  [propName: string]: number;
+  actor: number;
+  artist: number;
+  athlete: number;
+  crafter: number;
+  diplomat: number;
+  engineer: number;
+  fighter: number;
+  investigator: number;
+  linguist: number;
+  mage: number;
+  medic: number;
+  musician: number;
+  pilot: number;
+  scholar: number;
+  sharpshooter: number;
+  sneak: number;
+  stylist: number;
+  survivalist: number;
 }
 
 export class Skills {
@@ -13,7 +30,26 @@ export class Skills {
   }
 
   initialize(): SkillsData {
-    this._data = {};
+    this._data = {
+      actor: 0,
+      artist: 0,
+      athlete: 0,
+      crafter: 0,
+      diplomat: 0,
+      engineer: 0,
+      fighter: 0,
+      investigator: 0,
+      linguist: 0,
+      mage: 0,
+      medic: 0,
+      musician: 0,
+      pilot: 0,
+      scholar: 0,
+      sharpshooter: 0,
+      sneak: 0,
+      stylist: 0,
+      survivalist: 0
+    };
     return this._data;
   }
 
