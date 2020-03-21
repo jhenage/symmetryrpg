c048612f38c1cec12e927613c02eeadd1821b118
Thomas Henage <thenage@gmail.com>
2020-03-21 10:20:06 -0600

    reorganized component file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SheetComponent } from './sheet.component';
-import { MapComponent } from './map.component';
+import { SheetComponent } from './sheet/sheet.component';
+import { MapComponent } from './map/map.component';
 import { ExecuteComponent } from './log/execute/execute.component';
 import { LogComponent } from './log/log.component';
 
