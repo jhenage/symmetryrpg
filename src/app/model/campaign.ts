f2ca803cc1cb7fc8970459fc936ecefab9cd9ed7
Jon Henage <jonhenage@Jons-MacBook-Pro.local>
2021-05-05 09:45:49 -0600

    Adding scene layer

--- src/app/model/campaign.ts
+++ src/app/model/campaign.ts
@@ -1,13 +1,16 @@
-import { Character } from './character'
+import { Character } from './character';
+import { Scene, SceneData } from './scene';
 import { CreatureType, CreatureTypeData } from './creaturetype';
 import { SpecialtyCategories } from './character/specialties';
 import { TraitDetails } from './character/traits';
 import { CurrencyInterval } from './character/equipment';
+import { timeStamp } from 'console';
 
 export interface CampaignData {
-    characters: {id:number,version:number}[];
-    allCharacters: {name:string,versions:number[]}[];
+    scenes: SceneData[];
+    characterCount: number;
     now: number; // The current time
+    activeScene: number; // Index of the active scene
     creatureTypes: CreatureTypeData[];
     commonSpecialties: SpecialtyCategories; // each specialty should have no more than 4 categories
     traitsDetails: {
@@ -24,10 +27,10 @@ export class Campaign {
 
     protected _data: CampaignData;
 
-    characters: Character[];
+    scenes: Scene[];
     creatureTypes: CreatureType[];
 
-    constructor(data: CampaignData,getcharacter: (id,version)=>any) {
+    constructor(data: CampaignData,getcharacter: (id,scene)=>any) {
         
         this._data = data;
  
@@ -36,12 +39,10 @@ export class Campaign {
             this.creatureTypes.push(new CreatureType(creaturetype));
         }
 
-        this.characters = [];
-        for ( let char of data.characters ) {
-            let charData = getcharacter(char.id,char.version);
-            let charObject = new Character(char.id,charData.creatureType,charData,this);
-            // charObject.resetAll(); // For Debugging
-            this.characters.push(charObject);
+        this.scenes = [];
+        for ( let scene of data.scenes ) {
+            let sceneObject = new Scene(scene,this,getcharacter);
+            this.scenes.push(sceneObject);
         }
     }
 
@@ -65,6 +66,10 @@ export class Campaign {
         return this._data.currencyIntervals;
     }
 
+    get activeScene(): Scene {
+        return this.scenes[this._data.activeScene];
+    }
+
     getTraitList(traitType: string): string[] {
         return Object.keys(this._data.traitsDetails[traitType]);
     }
@@ -81,38 +86,19 @@ export class Campaign {
         return JSON.stringify(this._data);
     }
 
-    archiveCharacter(character: Character): void {
-        let index = this.characters.indexOf(character);
-        this.characters.splice(index,1);
-        this._data.characters.splice(index,1);
-        this._data.allCharacters[character.id].name = character.about.name;
-    }
-    deleteCharacter(character: Character): void {
-        this.archiveCharacter(character);
+//    archiveCharacter(character: Character): void {
+//        let index = this.characters.indexOf(character);
+//        this.characters.splice(index,1);
+//        this._data.characters.splice(index,1);
+//        this._data.allCharacters[character.id].name = character.about.name;
+//    }
+//    deleteCharacter(character: Character): void {
+//        this.archiveCharacter(character);
+//
+//    }
 
-    }
-
-    newCharacter(): Character {
-        let character = new Character(this._data.allCharacters.length,0,this._data.now,this);
-        this.characters.push(character);
-        this._data.characters.push({id:character.id,version:character.createdAt});
-        this._data.allCharacters.push({name:'',versions:[character.createdAt]});
-        return character;
-    }
-
-    nextActions(): any[] {
-        let actions = [];
-        let current = 0;
-        this.characters.forEach((char) => {
-            char.actions.nextActions(this._data.now).forEach((action) => {
-                if ( current == 0 || current > action.time ) {
-                    current = action.time;
-                }
-                actions.push(action);
-            });
-        });
-        
-        return actions.filter((action) => { return action.time == current; });
+    nextCharacter(): number {
+        return ++this._data.characterCount;
     }
 
 
