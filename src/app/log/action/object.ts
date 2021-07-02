b74c502eed5737af0417719d9260bf7c6de144d3
jhenage <jhenage@gmail.com>
2021-07-01 19:57:27 -0600

    Adding timing to actions

--- src/app/log/action/object.ts
+++ src/app/log/action/object.ts
@@ -26,4 +26,31 @@ export class ActionObject {
             }
         }
     }
+
+    protected getBaseReactionTime(symmetric:number): number { // return milliseconds
+        if(symmetric < -1) return 26 + 16 * symmetric * symmetric;
+        if(symmetric < 4) return 38 - 4 * symmetric;
+        if(symmetric < 7) return 30 - 2 * symmetric;
+        return Math.max(1,26.5 - 1.5 * symmetric);
+    }
+    getSurpriseMultiplier(symmetric: number): number {
+        if(symmetric < 0) return 10 - symmetric;
+        return Math.max(2, 10 - 0.4 * symmetric);
+    }
+
+
+    getPhysicalReactionTime(time: number, modifier: number, isSurprised: boolean): number { // return milliseconds
+        let result = this.getBaseReactionTime(this.character.aspects.current(time,'awareness') + 5 + modifier) * this.character.creatureType.quickness.mental;
+        result += this.getBaseReactionTime(this.character.aspects.current(time,'reflex') + modifier) * this.character.creatureType.quickness.physical;
+        if(isSurprised) result *= this.getSurpriseMultiplier(this.character.aspects.current(time,'reflex') + modifier);
+        return result;
+    }
+    
+
+    getMentalReactionTime(time: number, modifier: number, isSurprised: boolean): number { // return milliseconds
+        let result = this.getBaseReactionTime(this.character.aspects.current(time,'awareness') + modifier) * this.character.creatureType.quickness.mental;
+        result += this.getBaseReactionTime(this.character.aspects.current(time,'reflex') + 5 + modifier) * this.character.creatureType.quickness.physical;
+        if(isSurprised) result *= this.getSurpriseMultiplier(this.character.aspects.current(time,'awareness') + modifier);
+        return result;
+    }
 }
